d753366a52c00b47c18c84178ed49ee0c03f5d68
Loc Phan <phanhoangloc@gmail.com>
2018-03-16 07:55:03 +0700

    feat: add a slider for Progress

--- src/components/Styled/Text/Text.component.js
+++ src/components/Styled/Text/Text.component.js
@@ -1,10 +1,23 @@
 // @flow
 import { Text } from 'react-native'
 import glamorous from 'glamorous-native'
+import { keys, find } from 'ramda'
 
-const styles = (props, { colors }) => ({
-  color: colors.white,
-  fontSize: 14
-})
+const FONT_SIZE = 14
+const fontSizes = {
+  h1: 20,
+  h2: 18,
+  h3: 16
+}
+const styles = (props, { colors }) => {
+  const hasStyleInProps = key => props[key]
+  const fontStyle = find(hasStyleInProps, keys(fontSizes))
+  const fontSize =
+    (fontStyle && fontSizes[fontStyle]) || FONT_SIZE
+  return {
+    color: props.color || colors.white,
+    fontSize
+  }
+}
 
 export default glamorous(Text)(styles)
--- src/modules/GoalDetails/GoalDetails.view.js
+++ src/modules/GoalDetails/GoalDetails.view.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react'
-import { Button } from 'react-native'
+import { Button, View } from 'react-native'
 
 import Progress from '../../components/Progress/Progress.component'
 import { Wrapper, Text } from '../../components/Styled'
@@ -11,17 +11,15 @@ type Props = {
   REMOVE_GOAL: Function
 }
 
-// draw a progress bar
 const GoalDetailsView = ({
   REMOVE_GOAL,
   goalItem
 }: Props) => (
   <Wrapper>
-    <Text>{goalItem.title}</Text>
-    <Text>Start: {goalItem.start}</Text>
-    <Text>Current: {goalItem.current}</Text>
-    <Text>Target: {goalItem.end}</Text>
-    <Progress {...goalItem} />
+    <Text h1>{goalItem.title}</Text>
+    <View style={{ width: 200 }}>
+      <Progress {...goalItem} disabled />
+    </View>
     <Button
       title={I18n.t('goalDetails.view.buttons.delete')}
       onPress={REMOVE_GOAL}
