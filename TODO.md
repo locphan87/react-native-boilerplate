b84275ea2a0f49a1e24f4181a5ca2440b5142a65
Loc Phan <phanhoangloc@gmail.com>
2018-03-24 19:28:10 +0700

    fix: add coverage on CI (#44)
    * fix: add coverage on CI
    
    * fix: run codecov bash

--- TODO.md
+++ TODO.md
@@ -2,24 +2,15 @@
 
 ## General
 
-- [ ] Make the boilerplate less opinionated, give it more freedom to make changes
-- [ ] [docs] code structure
-- [ ] [docs] how does it works (ecosystem)
-
-## Set up
-
-- [ ] react native storage, redux-persist?
+- [ ] [docs] code structures & conventions
 
 ## Advanced
 
 - [ ] build a script to remove example code
 - [ ] fp libraries + babel plugins
 - [ ] graphql support
-- [ ] Offline support
 
 ## Others?
 
-- [ ] tcomb
-- [ ] immutable data structures
 - [ ] redux-loop
 - [ ] redux-saga
--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,9 @@ module.exports = {
   verbose: true,
   preset: 'jest-expo',
   coverageReporters: ['lcov', 'text', 'text-summary'],
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/flow-typed/'
-  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/flow-typed/'],
   moduleFileExtensions: ['js'],
-  testMatch: [
-    '**/__tests__/**/*.js',
-    '**/?(*.)(spec|test).js'
-  ],
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)(spec|test).js'],
   setupFiles: ['<rootDir>/tests/setup.js'],
   coveragePathIgnorePatterns: [
     '(style|story).js',
@@ -21,14 +15,6 @@ module.exports = {
   snapshotSerializers: ['enzyme-to-json/serializer'],
   clearMocks: true,
   timers: 'fake',
-  coverageThreshold: {
-    global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50
-    }
-  },
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|react-navigation|glamorous-native|@expo|expo)'
   ]
