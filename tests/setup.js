8b36fc2b8f3ccead13d6d236a93321f81c40762f
Loc Phan <phanhoangloc@gmail.com>
2018-04-02 18:43:34 +0700

    fix: unit tests

--- tests/setup.js
+++ tests/setup.js
@@ -3,12 +3,10 @@ import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
 
-jest
-  .mock('react-native-elements', () => {
-    const actual = require.requireActual('react-native-elements')
-    return {
-      ...actual,
-      ListItem: 'ListItem'
-    }
-  })
-  .mock('react-native-dotenv', () => ({}))
+jest.mock('react-native-elements', () => {
+  const actual = require.requireActual('react-native-elements')
+  return {
+    ...actual,
+    ListItem: 'ListItem'
+  }
+})
