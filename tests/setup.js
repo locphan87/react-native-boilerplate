5540c306201f6be196890fdf309ba619db4729a5
Loc Phan <phanhoangloc@gmail.com>
2018-04-02 18:36:37 +0700

    chore: add coverage threshold

--- tests/setup.js
+++ tests/setup.js
@@ -3,12 +3,12 @@ import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
 
-jest.mock('react-native-elements', () => {
-  const actual = require.requireActual(
-    'react-native-elements'
-  )
-  return {
-    ...actual,
-    ListItem: 'ListItem'
-  }
-})
+jest
+  .mock('react-native-elements', () => {
+    const actual = require.requireActual('react-native-elements')
+    return {
+      ...actual,
+      ListItem: 'ListItem'
+    }
+  })
+  .mock('react-native-dotenv', () => ({}))
