645fea746ef43d0b8a0232e528dc267190055c7f
Loc Phan <phanhoangloc@gmail.com>
2018-08-09 16:10:18 +0700

    fix: unit tests

--- tests/setup.js
+++ tests/setup.js
@@ -2,3 +2,7 @@ import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
+
+// fix wrong display name of view
+// ref: https://github.com/facebook/react-native/pull/18813
+jest.mock('View', () => 'View')
